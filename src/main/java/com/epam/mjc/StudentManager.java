a9469162d5171436c09e7834da99447627473b8f
ilaylyk <ilay_lyk@mail.ru>
2022-08-22 19:32:46 +0300

    Task Exceptions2

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -6,15 +6,25 @@ public class StudentManager {
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
   public Student find(long studentID) {
-    return Student.getValueOf(studentID);
+    Student res;
+
+    res = Student.getValueOf(studentID);
+    if (res==null)
+      throw new StudentManagerIllegalArgumentException("Could not find student with ID " + studentID);
+
+    return res;
   }
 
   public static void main(String[] args) {
     StudentManager manager = new StudentManager();
 
     for (int i = 0; i < IDs.length; i++) {
-      Student student = manager.find(IDs[i]);
-      System.out.println("Student name " + student.getName());
+      try {
+        Student student = manager.find(IDs[i]);
+
+        System.out.println("Student name " + student.getName());
+      } catch (NullPointerException ignored) {
+      }
     }
 
   }
---
+++ src/main/java/com/epam/mjc/StudentManagerIllegalArgumentException.java
@@ -0,0 +1,7 @@
+package com.epam.mjc;
+
+public class StudentManagerIllegalArgumentException extends IllegalArgumentException{
+    public StudentManagerIllegalArgumentException(String message){
+        super(message);
+    }
+}
